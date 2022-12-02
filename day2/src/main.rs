7f2dbbcf934bac4a16bad373ed44090ff6ff2ce8
baarkerlounger <db@slothlife.xyz>
2022-12-02 09:46:50 +0000

    Day 2

---
+++ day2/src/main.rs
@@ -0,0 +1,116 @@
+use std::fs;
+
+fn main() {
+    let filename = "input.txt";
+    println!(
+        "Total score, part 1 is {}",
+        strategy_outcome(filename, Part::One)
+    );
+    println!(
+        "Total score, part 2 is {}",
+        strategy_outcome(filename, Part::Two)
+    );
+}
+
+#[derive(Clone, PartialEq, Eq)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum Part {
+    One,
+    Two,
+}
+
+fn strategy_outcome(filename: &str, part: Part) -> u32 {
+    let file_contents =
+        fs::read_to_string(filename).expect("Should have been able to read the file");
+    let rounds = file_contents.trim().split("\n");
+    let mut score = 0;
+
+    for round in rounds {
+        let moves: Vec<&str> = round.split(" ").collect();
+        let opponent_move = decoded_move(moves[0]).unwrap();
+        let your_move = match part {
+            Part::One => decoded_move(moves[1]).unwrap(),
+            Part::Two => your_move(moves[1], &opponent_move).unwrap(),
+        };
+        score = score + shape_score(&your_move) + result(opponent_move, your_move);
+    }
+    score
+}
+
+fn decoded_move(encoded_move: &str) -> Result<Move, &'static str> {
+    match encoded_move {
+        "A" | "X" => Ok(Move::Rock),
+        "B" | "Y" => Ok(Move::Paper),
+        "C" | "Z" => Ok(Move::Scissors),
+        _ => Err("Invalid input"),
+    }
+}
+
+fn shape_score(shape: &Move) -> u32 {
+    match shape {
+        Move::Rock => 1,
+        Move::Paper => 2,
+        Move::Scissors => 3,
+    }
+}
+
+fn losing_move(opponent_move: &Move) -> Move {
+    match opponent_move {
+        Move::Rock => Move::Scissors,
+        Move::Paper => Move::Rock,
+        Move::Scissors => Move::Paper,
+    }
+}
+
+fn winning_move(opponent_move: &Move) -> Move {
+    match opponent_move {
+        Move::Rock => Move::Paper,
+        Move::Paper => Move::Scissors,
+        Move::Scissors => Move::Rock,
+    }
+}
+
+fn tying_move(opponent_move: &Move) -> Move {
+    opponent_move.clone()
+}
+
+fn result(opponent_move: Move, your_move: Move) -> u32 {
+    match your_move {
+        your_move if your_move == winning_move(&opponent_move) => 6,
+        your_move if your_move == losing_move(&opponent_move) => 0,
+        _ => 3,
+    }
+}
+
+fn your_move(result: &str, opponent_move: &Move) -> Result<Move, &'static str> {
+    match result {
+        "Y" => Ok(tying_move(opponent_move)),
+        "X" => Ok(losing_move(opponent_move)),
+        "Z" => Ok(winning_move(opponent_move)),
+        _ => Err("Invalid input"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strategy_outcome_part_1() {
+        let filename = "demo_input.txt";
+        let result = strategy_outcome(filename, Part::One);
+        assert_eq!(result, 15);
+    }
+
+    #[test]
+    fn test_strategy_outcome_part_2() {
+        let filename = "demo_input.txt";
+        let result = strategy_outcome(filename, Part::Two);
+        assert_eq!(result, 12);
+    }
+}
